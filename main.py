07cc676a725e2af72f01fde760bab2cd10c8dbf6
Harry <henry.iz@yandex.ru>
2024-04-28 20:57:58 +0300

    Added the mainloop

---
+++ main.py
@@ -0,0 +1,42 @@
+# Import modules
+import pygame
+import sys
+from pygame.locals import *
+import random
+import pathlib
+
+
+# Declare constants
+BACKGROUND_COLOR = (100, 0, 250)
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+FRAMES_PER_SECOND = 30
+
+# Initializing the environment
+pygame.init()
+window: pygame.Surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+# Initialize variables
+
+# Main gameloop
+while True:
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            pass
+
+
+
+    window.fill(BACKGROUND_COLOR)
+
+
+
+    pygame.display.update()
+    clock.tick(FRAMES_PER_SECOND)
+
+
+
